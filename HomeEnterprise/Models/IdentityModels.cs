4495b3e77e7d22b58833f1b9d60448f9054baa13
Amock8 <andre.cassar.b31279@mcast.edu.mt>
2020-04-15 15:47:01 +0200

    -Retreiving item type images in index -Added seeded data

--- HomeEnterprise/Models/IdentityModels.cs
+++ HomeEnterprise/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
@@ -36,17 +37,13 @@ namespace HomeEnterprise.Models
         protected override void Seed(ApplicationDbContext context)
         {
             base.Seed(context);
-            //Seed Qualities
-            List<Quality> qualities = new List<Quality>();
-            qualities.Add(new Quality() { Id = 1, QualityName = "Excellent" });
-            qualities.Add(new Quality() { Id = 2, QualityName = "Good" });
-            qualities.Add(new Quality() { Id = 3, QualityName = "Poor" });
-            qualities.Add(new Quality() { Id = 4, QualityName = "Bad" });
-            context.Qualities.AddRange(qualities);
+            //test
             //Seed Roles
             context.Roles.Add(new IdentityRole() { Id = "1", Name = "Admin" });
             context.Roles.Add(new IdentityRole() { Id = "2", Name = "Registered User" });
-            //seed Admin User
+            context.SaveChanges();
+
+            
             if (!(context.Users.Any(u => u.UserName == "admin@admin.com")))
             {
                 var userStore = new UserStore<ApplicationUser>(context);
@@ -55,6 +52,84 @@ namespace HomeEnterprise.Models
                 userManager.Create(userToInsert, "A@dmin1234");
                 userManager.AddToRole(userToInsert.Id, "admin");
             }
+            
+
+            for (int i = 1; i < 6; i++)
+            {
+                string username = "user" + i;
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var userToInsert = new ApplicationUser { UserName = username, Email = username+"@gmail.com" };
+                userManager.Create(userToInsert, username+"A@");
+                userManager.AddToRole(userToInsert.Id, "Registered User");
+                context.SaveChanges();
+                //userManager.AddToRole(userToInsert.Id, "admin");
+                //context.SaveChanges();
+            }
+
+            //Seed Categories
+            List<Category> categories = new List<Category>();
+            categories.Add(new Category() { Id = 1, CategoryName = "Electronics" });
+            categories.Add(new Category() { Id = 2, CategoryName = "Appliances" });
+            categories.Add(new Category() { Id = 3, CategoryName = "Vehicles" });
+            categories.Add(new Category() { Id = 4, CategoryName = "Gaming" });
+            categories.Add(new Category() { Id = 5, CategoryName = "Accessories" });
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+            //int count = 0;
+            foreach(Category c in categories)
+            {
+                for(int i = 1; i <= 20; i++)
+                {
+                    //count++;
+                    context.ItemTypes.Add(new ItemType() { TypeName = "type" + i, Image = "13c-BpIZvgWH7yQB3CbdP0_q5pHzTalIC", CategoryId = c.Id });
+                    //context.Items.Add(new Item() { ItemTypeId = i, OwnerId })
+                }
+            }
+            //var items = db.Items.Include(i => i.ItemType).Include(i => i.Owner).Include(i => i.Quality);
+            //return View(items.ToList());
+            //Seed Qualities
+            List<Quality> qualities = new List<Quality>();
+            qualities.Add(new Quality() { Id = 1, QualityName = "Excellent" });
+            qualities.Add(new Quality() { Id = 2, QualityName = "Good" });
+            qualities.Add(new Quality() { Id = 3, QualityName = "Poor" });
+            qualities.Add(new Quality() { Id = 4, QualityName = "Bad" });
+            context.Qualities.AddRange(qualities);
+
+            var users = context.Users;
+            List<string> uids = new List<string>();
+            foreach (ApplicationUser u in users)
+            {
+                uids.Add(u.Id);
+            }
+            string s = uids[0];
+            Random rnd = new Random();
+            long qid = 0;
+            for (int i = 1; i < 101; i++)
+            {
+                qid++;
+                if(qid > 5)
+                {
+                    qid = 1;
+                }
+                //context.Items.Add(new Item() { ItemTypeId = i,  OwnerId = uids[rnd.Next(0, 6)], Price = i, QualityId = Convert.ToInt64(uids[rnd.Next(0, 6)]), Quantity = i });
+                context.Items.Add(new Item() { ItemTypeId = i, OwnerId = uids[rnd.Next(0, 6)], Price = i, QualityId = (long)rnd.Next(1, 5), Quantity = i });
+            }
+            
+            //context.Users.All();
+            
+            
+            //seed Admin User
+            /*
+            if (!(context.Users.Any(u => u.UserName == "admin@admin.com")))
+            {
+                //var userStore = new UserStore<ApplicationUser>(context);
+                //var userManager = new UserManager<ApplicationUser>(userStore);
+                //var userToInsert = new ApplicationUser { UserName = "admin@admin.com", Email = "admin@admin.com" };
+                //userManager.Create(userToInsert, "A@dmin1234");
+                //userManager.AddToRole(userToInsert.Id, "admin");
+            }
+            */
         }
     }
 
